803c2ccf5c6e5370a8eba00d1dc3d8316d769912
Milan May'r <mmayr@signifyhealth.com>
2021-12-27 12:04:14 -0600

    Basic GO web service

--- paths/go-core/main.go
+++ paths/go-core/main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"go-webservice/models"
+	"go-webservice/controllers"
+	"net/http"
 )
 
 func main() {
-	u := models.User{
-		ID: 2,
-		FirstName: "Tricia",
-		LastName: "McMillan",
-	}
-	fmt.Println(u)
+	controllers.RegisterControllers()
+	http.ListenAndServe(":3000", nil)
 }
